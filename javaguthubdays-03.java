7733a88682ed7be3354c15ce067f0df16f91f265
serusharma <sharmasachin80505@gmail.com>
2024-10-01 22:52:56 +0530

    Add files via upload

---
+++ javaguthubdays-03.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+	    int a = 5;
+	    
+		// your code goes here
+
+	}
+}
